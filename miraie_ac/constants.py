231064d215ed2ecddef0cec6f0cb21664276e635
s1n7ax <srineshnisala@gmail.com>
2025-12-26 23:57:17 +0530

    Remove hardcoded httpClientId from constants

--- miraie_ac/constants.py
+++ miraie_ac/constants.py
@@ -1,8 +1,9 @@
-httpClientId = "PBcMcfG19njNCL8AOgvRzIC8AjQa"
+httpClientId = ""
 loginUrl = "https://auth.miraie.in/simplifi/v1/userManagement/login"
 homesUrl = "https://app.miraie.in/simplifi/v1/homeManagement/homes"
 statusUrl = "https://app.miraie.in/simplifi/v1/deviceManagement/devices/{deviceId}/mobile/status"
 deviceDetailsUrl = "https://app.miraie.in/simplifi/v1/deviceManagement/devices/deviceId"
-energyConsumptionUrl =  "https://app.miraie.in/simplifi/v1/powerConsumption/devices/{deviceId}?grain={periodType}&startDate={fromDate}&endDate={toDate}"
+energyConsumptionUrl = "https://app.miraie.in/simplifi/v1/powerConsumption/devices/{deviceId}?grain={periodType}&startDate={fromDate}&endDate={toDate}"
 
 PACKAGE_NAME = "custom_components.miraie"
+
